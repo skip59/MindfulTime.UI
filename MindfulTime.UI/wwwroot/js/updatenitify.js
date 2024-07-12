1f3a7bd5d8fdd706af649f8e6d5379364d8fa675
skip59 <skip59@yandex.ru>
2024-07-12 16:31:08 +0500

    Настройка сервиса уведомлений

---
+++ MindfulTime.UI/wwwroot/js/updatenitify.js
@@ -0,0 +1,49 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const sendMessageCheck = document.getElementById('sendMessageCheck');
+    const notificationOptions = document.getElementById('notificationOptions');
+    const notifyTelegram = document.getElementById('notifyTelegram');
+    const telegramIdInput = document.getElementById('tid');
+
+    sendMessageCheck.addEventListener('change', function () {
+        if (this.checked) {
+            notificationOptions.style.display = 'block';
+        } else {
+            notificationOptions.style.display = 'none';
+        }
+    });
+
+    if (sendMessageCheck.checked) {
+        notificationOptions.style.display = 'block';
+    }
+});
+
+$(function () {
+    $('#updateUserSettings').on('submit', function (event) {
+        event.preventDefault();
+
+        if ($('#notifyTelegram').is(':checked') && !$('#tid').val()) {
+            alert('Пожалуйста, введите ваш Telegram ID для получения уведомлений через Telegram.');
+            return;
+        }
+
+        const umail = $('#userEmail').val();
+        const uid = '00000000-0000-0000-0000-000000000000';
+        const tid = $('#tid').val();
+        const notificationOption = true;
+
+        $.ajax({
+            url: '/WorkSpace/UpdateUser',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                Id: uid,
+                Email: umail,
+                TelegramId: tid,
+                IsSendMessage: notificationOption
+            }),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+});
